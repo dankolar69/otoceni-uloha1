65e4ca6799107dfd8b1e79995f892859ee3c4808
dankolar69 <91247767+dankolar69@users.noreply.github.com>
2022-04-10 12:52:42 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+s = [2, 2]
+a = [2, 0]
+b = [4, 2]
+c = [0, 2]
+
+
+led.plot(s[0], s[1])
+led.plot(a[0], a[1])
+led.plot(b[0], b[1])
+led.plot(c[0], c[1])
+
+
+def otoceni(d):
+    global a, b, c
+    basic.clear_screen()
+    a.reverse()
+    a[d]= abs(a[d]-4)
+    b.reverse()
+    b[d]= abs(b[d]-4)
+    c.reverse()
+    c[d]= abs(c[d]-4)
+    led.plot(s[0], s[1])
+    led.plot(a[0], a[1])
+    led.plot(b[0], b[1])
+    led.plot(c[0], c[1])
+
+def on_button_pressed_a():
+    otoceni(0)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_b():
+    otoceni(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-basic.forever(function on_forever() {
+let s = [2, 2]
+let a = [2, 0]
+let b = [4, 2]
+let c = [0, 2]
+led.plot(s[0], s[1])
+led.plot(a[0], a[1])
+led.plot(b[0], b[1])
+led.plot(c[0], c[1])
+function otoceni(d: number) {
     
+    basic.clearScreen()
+    a.reverse()
+    a[d] = Math.abs(a[d] - 4)
+    b.reverse()
+    b[d] = Math.abs(b[d] - 4)
+    c.reverse()
+    c[d] = Math.abs(c[d] - 4)
+    led.plot(s[0], s[1])
+    led.plot(a[0], a[1])
+    led.plot(b[0], b[1])
+    led.plot(c[0], c[1])
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    otoceni(0)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    otoceni(1)
 })
